8bf4d885f42d56033ef9e17183115eb73d63246f
Andy Lulham <a.lulham@gmail.com>
2024-05-10 07:47:15 +0100

    Attempt to encrypt the secret

--- script.py
+++ script.py
@@ -1,5 +1,14 @@
+from base64 import b64encode
 from os import environ
+from nacl import encoding, public
 import requests
+
+def encrypt(public_key: str, secret_value: str) -> str:
+    """Encrypt a Unicode string using the public key."""
+    public_key = public.PublicKey(public_key.encode("utf-8"), encoding.Base64Encoder())
+    sealed_box = public.SealedBox(public_key)
+    encrypted = sealed_box.encrypt(secret_value.encode("utf-8"))
+    return b64encode(encrypted).decode("utf-8")
 
 
 token = environ["GH_TOKEN"]
@@ -9,7 +18,7 @@ headers = {
     "Accept": "application/vnd.github.v3+json"
 }
 payload = {
-    "encrypted_value": "TODO: encrypt it"
+    "encrypted_value": encrypt(environ["PUBLIC_KEY"], "TODO: encrypt it")
 }
 resp = requests.put(url, headers=headers, json=payload)
 resp.raise_for_status()
